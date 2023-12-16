38c80f4369938ce72402dc83393f77ac6c08c945
Jason Schuehlein <jason@schuehlein.eu>
2023-12-16 16:17:39 +0100

    add day 15 - rust

--- 2023/rust/src/bin/15_lens_library.rs
+++ 2023/rust/src/bin/15_lens_library.rs
@@ -1,16 +1,9 @@
 use std::fs;
 
-fn main() {
-    let mut contents: String =
-        fs::read_to_string("../15.in").expect("Should have been able to read the file");
-
-    // remove newline at end of file
-    contents = contents.trim_end().to_string();
-
-    let data: Vec<String> = parse(&contents);
-
-    solve1(&data);
-    solve2(&data);
+#[derive(Debug, Clone)]
+struct Item {
+    label: String,
+    focal_length: usize,
 }
 
 fn parse(data: &String) -> Vec<String> {
@@ -34,14 +27,85 @@ fn solve1(data: &Vec<String>) -> usize {
     // dbg!(&hash_values);
     let result: usize = hash_values.iter().sum();
 
-    println!("Part 1: {result}");
+
     result
 }
 
 fn solve2(data: &Vec<String>) -> usize {
-    0
+    // Create an array of length 256 with arrays of usize
+    let mut boxes: Vec<Vec<Item>> = vec![vec![]; 256];
+
+    // Loop over the instructions and fill the lens boxes
+    for instruction in data {
+
+        // ADD or REPLACE
+        if instruction.contains("=") {
+            let (label, focal_length) = instruction.split_once("=").unwrap();
+            let focal_length: usize = focal_length.parse().unwrap();
+            let box_id: usize = custom_hash(&label.to_string());
+
+            let item: Item = Item {
+                label: label.to_string(),
+                focal_length: focal_length,
+            };
+
+            // dbg!(&item);
+
+            let mut found: bool = false;
+            for slot_id in 0..boxes[box_id].len() {
+                if boxes[box_id][slot_id].label == label {
+                    // Replace
+                    boxes[box_id][slot_id].focal_length = focal_length;
+                    found = true;
+                    break;
+                }
+            }
+
+            if !found {
+                // Add
+                boxes[box_id].push(item);
+            }
+
+        // REMOVE
+        } else {
+            let label: String = instruction.split_once("-").unwrap().0.to_string();
+            let box_id: usize = custom_hash(&label);
+
+            for slot_id in 0..boxes[box_id].len() {
+                if boxes[box_id][slot_id].label == label {
+                    boxes[box_id].remove(slot_id);
+                    break;
+                }
+            }
+        }
+
+    }
+    dbg!(&boxes);
+
+    // Calculate the score
+    let mut result: usize = 0;
+    for box_id in 0..boxes.len() {
+        for slot_id in 0..boxes[box_id].len() {
+            result += (box_id+1) * (slot_id+1) * boxes[box_id][slot_id].focal_length;
+        }
+    }
+
+    result
 }
 
+fn main() {
+    let mut contents: String =
+        fs::read_to_string("../15.in").expect("Should have been able to read the file");
+
+    // remove newline at end of file
+    contents = contents.trim_end().to_string();
+
+    let data: Vec<String> = parse(&contents);
+
+    println!("Part 1: {}", solve1(&data));
+    println!("Part 2: {}", solve2(&data));
+
+}
 
 #[cfg(test)]
 mod tests {
@@ -59,10 +123,9 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_solve2() {
         let contents: String = String::from("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
         let testdata: Vec<String> = parse(&contents);
-        assert_eq!(solve2(&testdata), 0);
+        assert_eq!(solve2(&testdata), 145);
     }
 }
