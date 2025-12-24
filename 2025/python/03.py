4804ada42a44921df7cfe5c53d751ead05640431
Jason Schuehlein <jason@schuehlein.eu>
2025-12-24 13:55:21 +0100

    add day 03 part 2

--- 2025/python/03.py
+++ 2025/python/03.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import pytest
-import itertools
 
 test_input = """
 987654321111111
@@ -12,42 +11,48 @@ test_input = """
 class Bank:
     def __init__(self, blocks: str):
         self.batteries = [int(b) for b in blocks]
+        self.battery_count = len(self.batteries)
         self.battery_power_indices = {int(i): [] for i in range(10, 0, -1)}
         for idx, power in enumerate(self.batteries):
             self.battery_power_indices[power].append(idx)
 
-    def calculate_joltage(self) -> int:
-        first_power = -1
-        first_index = -1
-        second_power = -1
-        second_index = -1
+    def calculate_joltage(self, digits=2) -> int:
+        selected_powers = [-1] * digits
+        selected_indices = [-1] * digits
         for power in range(10, 0, -1):
-            indices = self.battery_power_indices[power]
-            for i in indices:
-                if first_index == -1 and i != len(self.batteries) - 1:
-                    first_power = power
-                    first_index = i
-                    continue
-                
-                if second_index == -1 and i > first_index:
-                    second_power = power
-                    second_index = i
-                    break
+            available_indices = self.battery_power_indices[power]
+            for current_index in available_indices:
+                for current_digit in range(digits):
+                    if selected_indices[current_digit] == -1:
+                        # Only up to length-(digits - current_digit) can be taken
+                        # and index cannot be already taken
+                        # and index must be greater than previous indices
+                        if (current_index <= len(self.batteries) - (digits - current_digit) 
+                                and current_index not in selected_indices
+                                and current_index > max(selected_indices[:current_digit], default=-1)):
+                            selected_powers[current_digit] = power
+                            selected_indices[current_digit] = current_index
+                            break
+        joltage = sum(power * (10 ** (digits - idx - 1)) for idx, power in enumerate(selected_powers))
+        return joltage
 
-        # print(f"First power: {first_power} at index {first_index}, Second power: {second_power} at index {second_index}")
-        return first_power * 10 + second_power
 
 def part_one(input_data: str) -> int:
     total_joltage = 0
     for bank in input_data.strip().splitlines():
         b = Bank(bank)
         joltage = b.calculate_joltage()
-        # print(f"Bank: {bank} => Joltage: {joltage}")
         total_joltage += joltage
     return total_joltage
 
+
 def part_two(input_data: str) -> int:
-    return 0 
+    total_joltage = 0
+    for bank in input_data.strip().splitlines():
+        b = Bank(bank)
+        joltage = b.calculate_joltage(digits=12)
+        total_joltage += joltage
+    return total_joltage
 
 
 @pytest.mark.parametrize(
@@ -60,7 +65,7 @@ def test_part_one(input_data: str, expected_output: int):
 
 @pytest.mark.parametrize(
     "input_data, expected_output", 
-    [(test_input, 0)]
+    [(test_input, 3121910778619)]
 )
 def test_part_two(input_data: str, expected_output: int):
     assert part_two(input_data) == expected_output
